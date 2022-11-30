e0755fd0a218eb3540991baaa5026f1f9e8f9913
amenesca <alanmenescal@hotmail.com>
2022-11-30 15:19:25 -0300

    agora sim

--- README.md
+++
@@ -1,4 +0,0 @@
-# push_swap_amenesca
-
-It's a programm that sorts a list of integers arguments using operations in a stack.
-compile with make and run like this ./push_swap 3 2 1
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: amenesca <amenesca@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 18:48:54 by amenesca          #+#    #+#             */
-/*   Updated: 2022/11/29 18:49:00 by amenesca         ###   ########.fr       */
+/*   Updated: 2022/11/30 15:17:45 by amenesca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,21 @@ int	main(int argc, char **argv)
 
 	x = 0;
 	limit = 0;
+	stack_a = NULL;
+	stack_b = NULL;
 	while (++x < argc && argc != 1)
 		ft_stackadd_back(&stack_a, ft_stacknew(push_atoi(argv[x], &limit)));
 	push_error(argc, argv, &stack_a, &limit);
 	prepare_for_sort(argc, &stack_a);
 	if ((argc - 1) > 5)
+	{
 		push_swap(&stack_a, &stack_b, (argc - 1));
-	else
+		free_stack(&stack_a);
+	}
+	if ((argc - 1) <= 5)
+	{
 		mini_push_swap(&stack_a, &stack_b, (argc - 1));
-	free_stack(&stack_a);
+		free_stack(&stack_a);
+	}
 	return (0);
 }
